6b55d78fcf5bbd25baba30027f73b5798751f8ed
GeneCannella <gene.cannella@gmail.com>
2017-12-21 16:06:07 -0500

    functional but not styled, also missing one timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,11 @@
     $(document).ready(function() {
 
         //declare global variables to hold session state
-        var indexOfQuestion;
+        var indexOfQuestion; //will hold a random number that is used to look into the questionsArray and get one Question
+
+        var questionsPerGame; //will hold the setting for number of questions asked before game ends
+        var questionsAsked; //will hold the running count of questions asked in one game
+
         var correctAnsCount, incorrectAnsCount, unansweredCount;
 
 
@@ -32,17 +36,25 @@
         }
 
         function initGame() {
-            console.log("init occured");
+            console.log("initGame was called");
+
+            //probably need to empty the gameBoard 
+            //$("#gameBoard").empty();
 
             //reset answer counts
             correctAnsCount = 0;
             incorrectAnsCount = 0;
             unansweredCount = 0;
 
+            questionsPerGame = 5;
+            questionsAsked = 0;
+
+            waitToStart();
+            
         }
 
         function waitToStart() {
-            console.log("waitToStart occurred");
+            console.log("waitToStart was called");
 
             //do stuff, then register a click handler for the start button
 
@@ -67,7 +79,11 @@
 
             //do stuff, then register a click handler for the answer buttons and a callback for the timeout
 
-            //do stuff:
+            //========================= "do stuff" begins ============================================================
+
+            //A question is being asked, so increment the running count of questions 
+            questionsAsked++;
+
             //empty the gameBoard div
             $("#gameBoard").empty();
 
@@ -87,9 +103,11 @@
                 var $choiceElement = $("<button type='button' class='answer btn btn-primary btn-lg btn-block text-left'></button>");
                 $choiceElement.attr("data-choice-num", i);
                 $choiceElement.text(questionsArray[indexOfQuestion][i]);
-                console.log($choiceElement);
+                //console.log($choiceElement);
                 $("#gameBoard").append($choiceElement);
             }
+
+            //========================== end of "do stuff" ============================================================
 
             //start a counter
             //display time remaining
@@ -106,60 +124,94 @@
 
 
         function nextStateFunc(a, b) {
+            console.log("nextStateFunc was called");
             var x = a; //placeholder for testing
             var y = b; //placeholder for testing
-            waitForAnswer();
+            console.log("y = " + y);
+            if (questionsAsked >= questionsPerGame) {
+                displaySessionStats();
+            } else {
+                waitForAnswer();
+            }
             return;
         }
 
-
         function displayAnswer() {
-            console.log("displayAnswer occurred");
+            console.log("displayAnswer was called");
             //empty the gameBoard div
-            $("#gameBoard").empty();            
-
+            $("#gameBoard").empty();
 
             //get the number of the choice made by the user
-            var clickedAnswerStr = $(this).attr("data-choice-num"); 
+            var clickedAnswerStr = $(this).attr("data-choice-num");
             console.log("clickedAnswerStr = " + clickedAnswerStr + " " + typeof clickedAnswerStr);
 
             //check if user choice is correct
             var resultStr;
             var indexOfAnswerStr = questionsArray[indexOfQuestion][5];
             if (clickedAnswerStr == indexOfAnswerStr) {
+                //set the text that will be displayed to indicate to user their choice was correct
                 resultStr = "Correct!";
+                //increment correct answers counter
+                correctAnsCount++;
+
             } else {
+                //set the text that will be displayed to indicate to user their choice was INcorrect
                 resultStr = "Incorrect!";
+                //increment incorrect answers counter 
+                incorrectAnsCount++; //need to handle unansweredCount
             }
 
             //display a right or wrong message in gameBoard div (don't actually need the id "result" so far)
             var resultBtnHTML = "<button id='result' type='button' class='btn btn-primary btn-lg btn-block'>" + resultStr + "</button>";
             $("#gameBoard").append(resultBtnHTML);
-            
+
             //display the correct answer message in gameBoard div
             var correctAnswer = questionsArray[indexOfQuestion][parseInt(indexOfAnswerStr)];
             console.log("correctAnswer = " + correctAnswer);
             var correctAnswerBtnHTML = "<button type='button' class='btn btn-primary btn-lg btn-block'>" + correctAnswer + "</button>";
             $("#gameBoard").append(correctAnswerBtnHTML);
-            
+
             //start a counter, but do not display time remaining
             //when timer expire call waitForAnswer if more questions, call displaySessionStats if no more questions
 
-            // example from w3Schls  myVar = setTimeout(function(){ alertFunc("First param", "Second param"); }, 2000);
-            timerVar = setTimeout(function(){ nextStateFunc("First param", "Second param"); }, 3000);
+            // example from w3Schls : myVar = setTimeout(function(){ alertFunc("First param", "Second param"); }, 2000);
+            timerVar = setTimeout(function() { nextStateFunc("First param", "Second param"); }, 1000);
 
             //return to main 
             return;
         }
 
-
-
-
         function displaySessionStats() {
+            console.log("displaySessionStats was called");
+
             //empty the gameBoard div
-            //display correctAnsCount, incorrectAnsCount, unansweredCount in gameBoard div
-            //display a play again button in gameBoard div
-            //return to main when start button clicked - do not call next state function
+            $("#gameBoard").empty();
+            
+            //display correctAnsCount, incorrectAnsCount, unansweredCount in console for testing 
+            console.log(correctAnsCount, incorrectAnsCount, unansweredCount);
+
+            //displaying the correctly anwsered count in gameBoard div
+            var correctlyAnsweredBtnHTML = "<button type='button' class='btn btn-primary btn-lg btn-block'>Correctly Answered: " + correctAnsCount + "</button>";
+            $("#gameBoard").append(correctlyAnsweredBtnHTML);
+
+            //displaying the incorrectly anwsered count in gameBoard div
+            var incorrectlyAnsweredBtnHTML = "<button type='button' class='btn btn-primary btn-lg btn-block'>Incorrectly Answered: " + incorrectAnsCount + "</button>";
+            $("#gameBoard").append(incorrectlyAnsweredBtnHTML);
+
+            //displaying the unanwsered count in gameBoard div
+            var unansweredBtnHTML = "<button type='button' class='btn btn-primary btn-lg btn-block'>Unanswered: " + unansweredCount + "</button>";
+            $("#gameBoard").append(unansweredBtnHTML);
+
+            //display a "play again" button in gameBoard div
+            var playAgainBtnHTML = "<button type='button' id='play-again' class='btn btn-primary btn-lg btn-block'>Play Again</button>";
+            $("#gameBoard").append(playAgainBtnHTML);
+          
+            $("#play-again").on("click", initGame);
+            console.log("Play Again click handler registered");
+            
+
+            return; //return from displaySessionStats
+
         }
 
 
@@ -171,7 +223,7 @@
         //end global function declarations =============================
 
         initGame();
-        waitToStart();
+
 
 
 
