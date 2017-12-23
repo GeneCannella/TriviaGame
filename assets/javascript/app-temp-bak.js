d827df05b87a0b27802220f116d23b0d1c51a42d
GeneCannella <gene.cannella@gmail.com>
2017-12-23 12:27:44 -0500

    put in real questions

--- assets/javascript/app-temp-bak.js
+++ assets/javascript/app-temp-bak.js
@@ -17,6 +17,9 @@
 
         //lets make the set of answers an array of answer objects
         var questionsArray = [
+            ["Fiat", "500-correct", "white", "blue", "grey", "1"],
+            ["Blah", "100", "two-correct", "four", "eight", "2"],
+            ["Meh", "777", "sweet", "sour-correct", "spicy", "3"],
             [
             "Which founding father is known for his large signature on the United States Declaration of Independence?",
             "Thomas Jefferson",
@@ -24,42 +27,6 @@
             "John Hancock",
             "John Adams",
             "3"
-            ],
-
-            [
-            "What was the name of the U.S. mail service, started in 1860, that used horses and riders?",
-            "Pony Express",
-            "Rider Mail",
-            "Fast Mail",
-            "U.S. Mail",
-            "1",
-            ],
-
-            [
-            "Who was the first US President to declare war?",
-            "Harry Truman",
-            "Richard Nixon",
-            "James Madison",
-            "John F. Kennedy",
-            "3",
-            ],
-
-            [
-            "Who assassinated President Abraham Lincoln?",
-            "John Wilkes Booth",
-            "Lee Harvey Oswald",
-            "James Earl Ray",
-            "Leon Czolgosz",
-            "1",
-            ],
-
-            [
-            "New York City was originally known by which Dutch name?",
-            "Cannes",
-            "Nieuw Amsterdam",
-            "Rouen",
-            "Biarritz",
-            "2",
             ],
 
         ];
